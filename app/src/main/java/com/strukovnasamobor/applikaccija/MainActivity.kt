281b28c1ca7c9d37e2c0e30677d7f71fdf8291e8
NSkeva <nikola.skeva1@gmail.com>
2022-05-05 14:43:09 +0200

    Kreirali smo projekt
    BACAM RUZE

---
+++ app/src/main/java/com/strukovnasamobor/applikaccija/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.strukovnasamobor.applikaccija
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
